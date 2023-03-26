c4544f689edca5d3940b1d83aa6ae4de0eb42d79
chiamaka28 <maduikechiamaka4@gmail.com>
2023-03-26 20:57:40 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import HeaderCard from "./components/HeaderCard";
+import Work from "./components/Work"
+
+
+function App() {
+  return (
+    <div className="container">
+     <HeaderCard />
+     <Work/>
+    </div>
+  );
+}
+
+export default App;
